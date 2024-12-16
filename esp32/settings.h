b39a04322da3c847fafb263a643e29b0c8ca365a
Christoph Helms <60834906+mrchrisster@users.noreply.github.com>
2024-12-16 08:29:31 -0800

    Update settings.h

--- esp32/settings.h
+++ esp32/settings.h
@@ -1,6 +1,6 @@
 // WiFi settings
-const char* ssid = "Panorama-2.4";
-const char* pass = "gr33kg3rman";
+const char* ssid = "";
+const char* pass = "";
 
 // Spotify settings
 String deviceName = "Van Baby";
