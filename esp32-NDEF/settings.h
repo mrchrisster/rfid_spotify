3a892929b806bcc0955631881e1a0891625c329d
Christoph Helms <60834906+mrchrisster@users.noreply.github.com>
2025-07-26 22:28:50 -0700

    Create settings.h

---
+++ esp32-NDEF/settings.h
@@ -0,0 +1,10 @@
+// WiFi settings
+const char* ssid = "EstuaryBack";
+const char* pass = "";
+
+// Spotify settings
+String deviceName = "Squamish Henrik Bluey";
+String clientId = "";
+String clientSecret = "";
+String refreshToken = "";
+
